745adfffc18dcd3010e3bd9fd7fa3e1ac1bdb120
Miyake Keiji <k.miyake01+dev@gmail.com>
2021-12-11 16:18:59 +0900

    feat: added components

---
+++ src/components/frame.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Frame = ({ children }) => {
+  return (
+    <div>
+      <h1>This is the page title</h1>
+      { children }
+    </div>
+  )
+}
+export default Frame
---
+++ src/pages/gallery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Frame from '../components/frame'
+
+const GalleryPage = () => {
+  return (
+    <Frame>
+      <p>This will be passed in as children</p>
+    </Frame>
+  )
+}
+
+export default GalleryPage
